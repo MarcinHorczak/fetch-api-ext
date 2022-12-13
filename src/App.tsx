d3139890a6b7318bce656b2d5bc225c98e0094b2
Marcin Horczak <marcin.horczak@euvic.pl>
2022-12-13 15:33:38 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  const onApiFetch = () => {};
+
+  return (
+    <div className="app">
+      <h1>Fetch API Ext.</h1>
+      <button onClick={onApiFetch}>Fetch data</button>
+    </div>
+  );
+};
+
+export default App;
