72f656030a6c80227c3709d6fa6074476b8f4cb7
Marcin Horczak <marcin.horczak@euvic.pl>
2022-12-13 15:49:56 +0100

    feat: random API data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
+
 import "./App.css";
 
+const API_URL = "https://catfact.ninja/fact";
+
 const App = () => {
-  const onApiFetch = () => {};
+  const [data, setData] = useState(null);
+
+  const onApiFetch = async () => {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    setData(data);
+  };
 
   return (
     <div className="app">
       <h1>Fetch API Ext.</h1>
-      <button onClick={onApiFetch}>Fetch data</button>
+      {data ? (
+        JSON.stringify(data)
+      ) : (
+        <button onClick={onApiFetch}>Fetch data</button>
+      )}
     </div>
   );
 };
